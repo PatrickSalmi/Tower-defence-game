65426a0b594cefe3868fd7fca08699e8baf05f48
Patrick Salmi <patrick.salmi@helsinki.fi>
2023-04-04 22:30:06 +0300

    added first test and coverage report

---
+++ src/tests/enemies_test.py
@@ -0,0 +1,13 @@
+import unittest
+from sprites.enemies import Enemies
+
+class TestEnemies(unittest.TestCase):
+    
+    def setUp(self):
+        self.enemy = Enemies((100,200))
+
+    def test_correct_starting_position(self):
+        # x = 100 - 50(image width) / 2 = 75
+        # y = 200 - 86(image heigth) / 2 = 157
+        self.assertEqual(self.enemy.x, 75)
+        self.assertEqual(self.enemy.y, 157)
